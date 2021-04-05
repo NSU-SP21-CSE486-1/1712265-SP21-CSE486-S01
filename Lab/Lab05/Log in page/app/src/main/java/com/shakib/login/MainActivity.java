e2697da9abab88088fc2214ee019c903b6cff608
syed Shakib <syed.shakib75@gmail.com>
2021-04-05 10:40:52 +0600

    Lab 05

---
+++ Lab/Lab05/Log in page/app/src/main/java/com/shakib/login/MainActivity.java
@@ -0,0 +1,18 @@
+package com.shakib.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Button login;
+    EditText username, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login= (Button)findViewById(R.id.Login);
+        us
+    }
+}
