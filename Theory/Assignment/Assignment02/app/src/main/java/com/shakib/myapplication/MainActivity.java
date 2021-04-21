9ddc68f1db375c6c6b9594afdfb8798c5878e9c4
syed Shakib <syed.shakib75@gmail.com>
2021-04-21 14:16:47 +0600

    Assignment -02

--- Theory/Assignment/Assignment02/app/src/main/java/com/shakib/myapplication/MainActivity.java
+++ Theory/Assignment/Assignment02/app/src/main/java/com/shakib/myapplication/MainActivity.java
@@ -2,28 +2,42 @@ package com.shakib.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] deptNames;
-    private Spinner spinnerDeptNames;
+
+    private ImageView imageview;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        deptNames = getResources().getStringArray(R.array.spinnerDeptName);
-        spinnerDeptNames = findViewById(R.id.spinnerDeptNameId);
-
-        ArrayAdapter<String>deptNameAdapter = new ArrayAdapter<String>(this,R.layout.spinnerview,R.id.SpinnerTextView,  deptNames);
-        spinnerDeptNames.setAdapter(deptNameAdapter);
-    }
+        imageview= findViewById(R.id.imageViewnext);
 
 
 
-    }
+
+        imageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SchoolActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+        } }
--- Theory/Assignment/Assignment02/app/src/main/java/com/shakib/myapplication/SchoolActivity.java
+++ Theory/Assignment/Assignment02/app/src/main/java/com/shakib/myapplication/SchoolActivity.java
@@ -3,12 +3,26 @@ package com.shakib.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SchoolActivity extends AppCompatActivity {
+
+    private Spinner deptNames;
+    String[] spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school);
+
+        spinner = getResources().getStringArray(R.array.DeptName);
+        deptNames = findViewById(R.id.spinnerId);
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinnerview,R.id.SpinnerTextView,spinner);
+        deptNames.setAdapter(spinnerAdapter);
+
+
+
     }
 }
