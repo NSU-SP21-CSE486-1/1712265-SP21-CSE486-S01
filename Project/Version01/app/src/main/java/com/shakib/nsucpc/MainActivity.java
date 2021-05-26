d2344fde727864525009abef784c11d6fce210c1
syed Shakib <syed.shakib75@gmail.com>
2021-05-26 17:13:45 +0600

    Project

--- Project/Version01/app/src/main/java/com/shakib/nsucpc/MainActivity.java
+++ Project/Version01/app/src/main/java/com/shakib/nsucpc/MainActivity.java
@@ -1,14 +1,55 @@
 package com.shakib.nsucpc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private ImageButton studentButton;
+    private RelativeLayout adminButton;
+    private RelativeLayout studentButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        studentButton = findViewById(R.id.studentButtonId);
+        adminButton = findViewById(R.id.adminButtonId);
+
+        studentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, studentLogin.class);
+                startActivity(intent);
+
+            }
+        });
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, adminLogin.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
+
+
+
+
 }
---
+++ Project/Version01/app/src/main/java/com/shakib/nsucpc/studentLogin.java
@@ -0,0 +1,161 @@
+package com.shakib.nsucpc;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class studentLogin extends AppCompatActivity  implements View.OnClickListener {
+
+    private EditText singinEmail;
+    private EditText singinPassword;
+    private ImageView singinButton;
+    private Button savedLoginButton;
+
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+    private CheckBox rememberMe;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+        this.setTitle("Firebase Login");
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        singinEmail = findViewById(R.id.singInEmailId);
+        singinPassword = findViewById(R.id.signInPasswordID);
+        singinButton = findViewById(R.id.sinInButtonID);
+
+
+        progressBar = findViewById(R.id.progressbarID);
+        rememberMe = findViewById(R.id.checkBoxRemembermeID);
+        savedLoginButton = findViewById(R.id.savedSingInButtonID);
+
+
+        singinButton.setOnClickListener(this);
+        savedLoginButton.setOnClickListener(this);
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+
+        switch (v.getId()){
+            case R.id.sinInButtonID:
+                userLogin();
+                break;
+
+
+            case R.id.savedSingInButtonID:
+                SharedPreferences sharedPreferences = getSharedPreferences("UserDetails", Context.MODE_PRIVATE);
+                if (sharedPreferences.contains("emailKey") && sharedPreferences.contains("passwordKey")){
+                    String userEmail = sharedPreferences.getString("emailKey","Data not Found");
+                    String userId = sharedPreferences.getString("passwordKey","Data not Found");
+                    singinEmail.setText(userEmail);
+                    singinPassword.setText(userId);
+
+                }
+
+        }
+
+
+    }
+
+    private void userLogin() {
+        String email = singinEmail.getText().toString().trim();
+
+        String password = singinPassword.getText().toString().trim();
+
+        //Saving data Using SharedPreference.
+
+        if (rememberMe.isChecked()){
+            String emailRememberme = singinEmail.getText().toString().trim();
+
+            String passwordRememberme = singinPassword.getText().toString().trim();
+            SharedPreferences sharedPreferences = getSharedPreferences("UserDetails", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("emailKey",emailRememberme);
+            editor.putString("passwordKey",password);
+            editor.commit();
+        }
+        else {}
+
+
+
+        if(email.isEmpty())
+        {
+            singinEmail.setError("Enter an email address");
+            singinEmail.requestFocus();
+            return;
+        }
+
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            singinEmail.setError("Enter a valid email address");
+            singinEmail.requestFocus();
+            return;
+        }
+
+        //checking the validity of the password
+        if(password.isEmpty())
+        {
+            singinPassword.setError("Enter a password");
+            singinPassword.requestFocus();
+            return;
+        }
+
+        if(password.length()< 4 )
+        {
+            singinPassword.setError("Password too short !! ");
+            singinPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(getApplicationContext(),AvailableJobs.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Login Unsuccesfull",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+}
