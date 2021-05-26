24ce55b9b50e3ddeb80015934d1fc2acd51f4f21
syed Shakib <syed.shakib75@gmail.com>
2021-05-26 20:55:16 +0600

    e2

--- Project/Version01/app/src/main/java/com/shakib/nsucpc/postJobs.java
+++ Project/Version01/app/src/main/java/com/shakib/nsucpc/postJobs.java
@@ -2,13 +2,30 @@ package com.shakib.nsucpc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class postJobs extends AppCompatActivity {
-
+    private Spinner schoolNames;
+    private DatePickerDialog datePickerDialog;
+    private Button deadlineDatePicker;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_jobs);
+
+
+
+
+
     }
-}
+
+    }
